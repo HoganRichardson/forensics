116ac762c8ddb892a692391547e3ba43d6d52fc9
Hogan Richardson <h.richardson@student.unsw.edu.au>
2018-08-08 16:47:28 +1000

    Added significant configs

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Test Post",
+        "excerpt":"Testing out a blog post. Here’s some code to try out #import &lt;stdio.h&gt;int main (void) {\tprintf(\"C is the best!\\n\");\treturn 0;}The End ! ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test-post/",
+        "teaser":null}]
